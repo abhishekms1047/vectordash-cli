84ecb046aee97a07eaaeffe94a063fdeb61e69fc
Arbaz Khatib <arbaz.khatib@gmail.com>
2018-03-10 16:20:07 -0500

    Update list.py

--- vectordash/list.py
+++ vectordash/list.py
@@ -10,25 +10,34 @@ import os
 def list_machines():
     """Retrieves JSON object from vectordash using secret user token and displays the list."""
     try:
-        if os.path.isfile('./secret_token'):
-            with open('./secret_token') as f:
+        filename = "./vectordash_config/secret_token.txt"
+        if os.path.isfile(filename):
+            with open(filename) as f:
                 secret_token = f.readline()
                 # print(secret_token)
                 full_url = "https://84119199.ngrok.io/api/list_machines/" + secret_token
                 # print(full_url)
                 try:
                     r = requests.get(full_url)
-                    data = r.json()
 
-                    for key, value in data.items():
-                        machine = "[" + key + "]: " + value['name']
-                        print(machine)
+                    if r.status_code == 200:
+                        data = r.json()
+
+                        for key, value in data.items():
+                            machine = "[" + key + "]: " + value['name']
+                            print(machine)
+                    else:
+                        print("Could not connect to vectordash API with provided token")
+
                 except json.decoder.JSONDecodeError:
                     print("Invalid token value. Please make sure you are using the most recently generated token.")
+
         else:
             print("Please make sure a valid token is stored. Run vectordash secret <token>")
+
     except TypeError:
-        print("Please make sure a valid token is stored. Run vectordash secret <token>")
+        print("Please make sure a valid token is stored. Run 'vectordash secret <token>'")
+
 
 
 if __name__ == '__main__':
